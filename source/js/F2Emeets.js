3b80999abe3fd6e255832c3dd6a93469e3c4d8bf
bell881122 <51412772+bell881122@users.noreply.github.com>
2019-07-09 00:54:11 +0800

    20190709 upload

---
+++ source/js/F2Emeets.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+
+    $('.carousel').carousel({
+        interval: 3000
+    })
+
+    $(".light-box").click(function (e) {
+        e.preventDefault();
+        var link = $(this).find("a").attr("href");
+        $(".embed-responsive-item").attr("src", link);
+    });
+
+    $(".hover-animated").mouseover(function () {
+        // $(".hover-show").toggleClass("hover-show");
+
+        $(this).find(".hover-show").addClass("fadeIn");
+        $(".hover-show-text").attr("style", "transform: translateY(0)");
+    });
+    $(".hover-animated").mouseout(function () {
+        // $(".hover-show").toggleClass("hover-show");
+        $(".hover-show-text").attr("style", "transform: translateY(10px)");
+        $(".hover-show").removeClass("fadeIn");
+        // e.stopPropagation();
+    });
+
+
+    $(".clickdown").click(function () {
+        $(this).parent().parent().find(".clickdown-menu").slideToggle();
+
+        // $(this).find(".hover-show").addClass("fadeIn");
+        // $(".hover-show-text").attr("style", "transform: translateY(0)");
+    });
+
+    $('#webdesign, #uxui, #react').click(function (e) {
+        e.preventDefault();
+        var tab = this.id;
+        var tabClass = '.' + tab + '';
+        $("#tab-pane").children().hide();
+        $("#tab-pane").find(tabClass).slideDown("slow");
+    });
+    $('#all').click(function (e) {
+        e.preventDefault();
+        var tab = this.id;
+        var tabClass = '.' + tab + '';
+        $("#tab-pane").children().hide();
+        $("#tab-pane").children().slideDown("slow");
+    });
+        // alert(tabClass);
+        // 
+
+
+        // console.log();
+
+
+
+});
+
